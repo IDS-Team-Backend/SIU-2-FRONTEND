55090730f72f5104ec24bacad39fd79e0a57b1a7
Nicolas Martinez <nicolasmartinezalfonso@gmail.com>
2026-06-04 18:05:20 -0300

    Rework: Se le agrego a la url /curso/id/{ruta}

--- frontend/routes/equipos_routes.py
+++ frontend/routes/equipos_routes.py
@@ -19,22 +19,24 @@ from services.alumnos_service import (
     buscar_alumno_por_padron,
 )
 
+from utils.filtros import url_con_filtros
+
 equipos_bp = Blueprint("equipos", __name__)
 
 CURSO_ACTIVO_ID = int(os.getenv("CURSO_ACTIVO_ID", "1"))
 
 
-@equipos_bp.route("/equipos", methods=["GET"])
+@equipos_bp.route("/curso/<int:curso_id>/equipos", methods=["GET"])
 @requiere_staff
-def listar_equipos():
-    ok_equipos, equipos = obtener_equipos()
+def listar_equipos(curso_id):
+    ok_equipos, equipos = obtener_equipos(curso_id)
 
     if not ok_equipos:
         flash(equipos, "danger")
         equipos = []
 
     ok_evals, evaluaciones = obtener_evaluaciones_del_curso(
-        CURSO_ACTIVO_ID
+        curso_id,
     )
 
     if not ok_evals:
@@ -44,20 +46,20 @@ def listar_equipos():
     return render_template(
         "equipos.html",
         title="Equipos",
+        curso_id=curso_id,
         active_page="equipos",
         equipos=equipos,
         evaluaciones=evaluaciones,
     )
 
-
-@equipos_bp.route("/equipos", methods=["POST"])
+@equipos_bp.route("/curso/<int:curso_id>/equipos", methods=["POST"])
 @requiere_staff
-def crear_nuevo_equipo():
+def crear_nuevo_equipo(curso_id):
     nombre = request.form.get("nombre", "").strip()
     evaluacion_id = request.form.get("evaluacion_id")
 
     ok, resultado = crear_equipo(
-        CURSO_ACTIVO_ID,
+        curso_id,
         evaluacion_id,
         nombre
     )
@@ -68,19 +70,21 @@ def crear_nuevo_equipo():
         flash(resultado, "danger")
 
     return redirect(
-        url_for("equipos.listar_equipos")
+        url_for(
+            "equipos.listar_equipos",
+            curso_id=curso_id
+        )
     )
 
-
-@equipos_bp.route("/equipos/<int:equipo_id>/actualizar", methods=["POST"])
+@equipos_bp.route("/curso/<int:curso_id>/equipos/<int:equipo_id>/actualizar", methods=["POST"])
 @requiere_staff
-def actualizar(equipo_id):
+def actualizar(curso_id,equipo_id):
     nombre = request.form.get("nombre", "").strip()
     activo = request.form.get("activo") == "on"
     evaluacion_id = request.form.get('evaluacion_id')
 
     ok, resultado = actualizar_equipo(
-        CURSO_ACTIVO_ID,
+        curso_id,
         equipo_id,
         evaluacion_id,
         nombre,
@@ -93,13 +97,15 @@ def actualizar(equipo_id):
         flash(resultado, "danger")
 
     return redirect(
-        url_for("equipos.listar_equipos")
+        url_for(
+            "equipos.listar_equipos",
+            curso_id=curso_id
+        )
     )
 
-
-@equipos_bp.route("/equipos/<int:equipo_id>/eliminar", methods=["POST"])
+@equipos_bp.route("/curso/<int:curso_id>/equipos/<int:equipo_id>/eliminar", methods=["POST"])
 @requiere_staff
-def eliminar(equipo_id):
+def eliminar(curso_id, equipo_id):
     ok, resultado = eliminar_equipo(equipo_id)
 
     if ok:
@@ -108,12 +114,15 @@ def eliminar(equipo_id):
         flash(resultado, "danger")
 
     return redirect(
-        url_for("equipos.listar_equipos")
+        url_for(
+            "equipos.listar_equipos",
+            curso_id=curso_id
+        )
     )
 
-@equipos_bp.route("/equipos/<int:equipo_id>/integrantes", methods=["GET"])
+@equipos_bp.route("/curso/<int:curso_id>/equipos/<int:equipo_id>/integrantes", methods=["GET"])
 @requiere_staff
-def administrar_integrantes(equipo_id):
+def administrar_integrantes(curso_id, equipo_id):
     ok, integrantes = obtener_integrantes(equipo_id)
 
     if not ok:
@@ -124,17 +133,14 @@ def administrar_integrantes(equipo_id):
         "equipo_integrantes.html",
         title="Integrantes del Equipo",
         active_page="equipos",
+        curso_id=curso_id,
         equipo_id=equipo_id,
         integrantes=integrantes,
     )
 
-
-@equipos_bp.route(
-    "/equipos/<int:equipo_id>/buscar-integrante",
-    methods=["POST"]
-)
+@equipos_bp.route("/curso/<int:curso_id>/equipos/<int:equipo_id>/buscar-integrante", methods=["POST"])
 @requiere_staff
-def buscar_integrante(equipo_id):
+def buscar_integrante(curso_id, equipo_id):
     padron = request.form.get("padron", "").strip()
     ok, alumno = buscar_alumno_por_padron(padron)
     if not ok:
@@ -143,7 +149,8 @@ def buscar_integrante(equipo_id):
         return redirect(
             url_for(
                 "equipos.administrar_integrantes",
-                equipo_id=equipo_id
+                equipo_id=equipo_id,
+                curso_id=curso_id
             )
         )
 
@@ -163,17 +170,15 @@ def buscar_integrante(equipo_id):
     return redirect(
         url_for(
             "equipos.administrar_integrantes",
-            equipo_id=equipo_id
+            equipo_id=equipo_id,
+            curso_id=curso_id
         )
     )
 
 
-@equipos_bp.route(
-    "/equipos/<int:equipo_id>/integrantes/<int:alumno_id>/eliminar",
-    methods=["POST"]
-)
+@equipos_bp.route("/curso/<int:curso_id>/equipos/<int:equipo_id>/integrantes/<int:alumno_id>/eliminar", methods=["POST"])
 @requiere_staff
-def eliminar_integrante_equipo(equipo_id, alumno_id):
+def eliminar_integrante_equipo(curso_id, equipo_id, alumno_id):
     ok, resultado = eliminar_integrante(
         equipo_id,
         alumno_id
@@ -189,6 +194,7 @@ def eliminar_integrante_equipo(equipo_id, alumno_id):
     return redirect(
         url_for(
             "equipos.administrar_integrantes",
-            equipo_id=equipo_id
+            equipo_id=equipo_id,
+            curso_id=curso_id
         )
     )
